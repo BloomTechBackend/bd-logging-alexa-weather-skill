98d8463e8287aa749d6a5be8a5ac09bc85d03e15
Frank Fella <frank.fella@lambdaschool.com>
2021-10-15 12:45:09 -0400

    FFella Push 10/15/2021 12:45:09

--- src/com/amazon/ata/remotedebuggingandlogging/LambdaMain.java
+++ src/com/amazon/ata/remotedebuggingandlogging/LambdaMain.java
@@ -1,18 +1,22 @@
 package com.amazon.ata.remotedebuggingandlogging;
 
-import com.amazon.coral.metrics.MetricsFactory;
-import com.amazon.coral.metrics.NullMetricsFactory;
-import com.amazon.coral.service.lambda.ApiGatewayRequest;
-import com.amazon.coral.service.lambda.ApiGatewayResponse;
-import com.amazon.coral.service.lambda.LambdaEndpoint;
-import com.amazon.coral.service.lambda.LambdaEndpointConfig;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+// import com.amazon.coral.metrics.MetricsFactory;
+import com.amazon.coral.*; // Frank
+//import com.amazon.coral.metrics.NullMetricsFactory;
+//import com.amazon.coral.service.lambda.ApiGatewayRequest;
+//import com.amazon.coral.service.lambda.ApiGatewayResponse;
+//import com.amazon.coral.service.lambda.LambdaEndpoint;
+//import com.amazon.coral.service.lambda.LambdaEndpointConfig;
+//import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.*;
+//import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.*;
+//import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.*;
 
-import static com.amazon.coral.service.lambda.LambdaEndpointConfig.Protocol.rest;
-import static com.amazon.coral.service.lambda.LambdaEndpointConfig.SerializationFormat.AWS_JSON_11;
+import static com.amazon.coral.*; // service.lambda.*; // LambdaEndpointConfig.*; // .Protocol.rest;
+import static com.amazon.coral.*; // service.lambda.*; // LambdaEndpointConfig.*; // SerializationFormat.AWS_JSON_11;
 
 public class LambdaMain implements RequestHandler<ApiGatewayRequest, ApiGatewayResponse> {
     private static final Logger LOG = LogManager.getLogger(LambdaMain.class);
